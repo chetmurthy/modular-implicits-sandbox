23e5d423158814ec2bd36f0f38b694272691f756
Chet Murthy <chetsky@gmail.com>
2022-08-26 10:46:03 -0700

    meh

--- tests/core_test.ml
+++ tests/core_test.ml
@@ -2,9 +2,15 @@
 (* core_test.ml *)
 
 open OUnit2
+open Core_ops
 
 let test_simple ctxt =
-  ()
+  assert_equal (-1.) (neg 1.)
+  ; assert_equal (-1) (neg 1)
+  ; assert_equal Complex.(neg one) (neg Complex.one)
+  ; assert_equal 2 (1 + 1)
+  ; assert_equal 2. (1. + 1.)
+  ; assert_equal Complex.one (Complex.one + Complex.zero)
 ;;
 
 let suite = "core_opps" >::: [
